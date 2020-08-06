d45e2f4d0d9f22a11442d8e3c2d8999bc0984b48
megawatts68 <andrew.f.watts@gmail.com>
2020-08-07 06:02:43 +0700

    Starting

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,12 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 test("renders App without crashing", () => {
   render(<App />);
 });
+
+test('Can fill out form, form validates, click submit and object appears')
+  render(<App /> )
+
+  const fName = screen.getByLabelText()
